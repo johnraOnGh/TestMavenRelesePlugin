4c5d51c7026d6b79904882a748c4cd4cdba20812
johnraOnGh <45717798+johnraOnGh@users.noreply.github.com>
2018-12-09 00:28:02 +0000

    new files

---
+++ src/main/java/Version.java
@@ -0,0 +1,10 @@
+public class Version {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("");
+        System.out.println("This is verison" + Version.class.getPackage().getImplementationVersion());
+    }
+
+
+}
